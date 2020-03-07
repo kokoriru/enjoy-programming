46cfdbc025572ad12f9bbc6085fecf7407e351ed
kokoriru <yuka-ogihara@hitotomirai.co.jp>
2020-03-08 00:06:20 +0900

    consider when params is nil

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,13 @@
 class ExercisesController < ApplicationController
   def q1
+    set_product
+  end
+
+  private
+
+  def set_product
+    return if params[:a].blank? && params[:b].blank?
+
     @a = params[:a].to_i if params[:a].to_i >= 1
     @b = params[:b].to_i if params[:b].to_i <= 10_000
     @product = @a * @b
