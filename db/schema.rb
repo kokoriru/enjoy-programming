b5325467e2bd72b45737c45a48ad51f9d7b09da4
kokoriru <yuka-ogihara@hitotomirai.co.jp>
2020-03-07 17:33:12 +0900

    execute [rails db:rollback]

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_034627) do
-
-  create_table "a_product20180121s", force: :cascade do |t|
-    t.integer "a"
-    t.integer "b"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_03_06_154730) do
 
   create_table "ps", force: :cascade do |t|
     t.string "title"
